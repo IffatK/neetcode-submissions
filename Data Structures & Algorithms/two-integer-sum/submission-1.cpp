69afaab01683e414b90057e973a95f007d08a17d
Iffat Khan <123642351+IffatK@users.noreply.github.com>
2026-07-16 19:01:25 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int sum=0;
+        int num1,num2;
+        for(int i = 0; i<nums.size();i++){
+            for(int j = i + 1;j<nums.size();j++){
+                sum = nums[i] + nums[j];
+                if(sum == target){
+                  num1=i;
+                  num2=j;
+                  return {num1,num2};
+                }
+            }
+        }
+         return {num1,num2};
+    }
+   
+};
